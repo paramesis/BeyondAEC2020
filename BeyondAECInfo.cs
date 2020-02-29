43df9317569859c791316f526fd9014356855ed6
paramesis <adamsdan24@gmail.com>
2020-02-29 00:46:24 -0500

    Initial Commit

---
+++ BeyondAECInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace BeyondAEC
+{
+    public class BeyondAECInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "RoomPlacer";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("f60ee0b7-67f3-4b9b-ae7b-72b0a791214e");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
---
+++ RoomPlacer.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace BeyondAEC
+{
+    public class RoomPlacer : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public RoomPlacer()
+          : base("RoomPlacer", "RP",
+              "Place rooms around a curve",
+              "BeyondAEC", "MintyFresh")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            // Use the pManager object to register your input parameters.
+            // You can often supply default values when creating parameters.
+            // All parameters must have the correct access type. If you want 
+            pManager.AddRectangleParameter("Rectangle", "rect", "rectangles", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Curve", "C", "The Curve", GH_ParamAccess.item);
+
+            // If you want to change properties of certain parameters, 
+            // you can use the pManager instance to access them by index:
+            //pManager[0].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            // Use the pManager object to register your output parameters.
+            // Output parameters do not have default values, but they too must have the correct access type.
+            pManager.AddCurveParameter("Output", "O", "Output Curves", GH_ParamAccess.list);
+            pManager.AddPointParameter("Test", "T", "Test Points", GH_ParamAccess.list);
+            // Sometimes you want to hide a specific parameter from the Rhino preview.
+            // You can use the HideParameter() method as a quick way:
+            //pManager.HideParameter(0);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            // First, we need to retrieve all data from the input parameters.
+            // We'll start by declaring variables and assigning them starting values.
+
+            List<Rectangle3d> rect_list = new List<Rectangle3d>();
+            Curve footprint = null;
+
+            if (!DA.GetDataList(0, rect_list)) return;
+            if (!DA.GetData(1, ref footprint)) return;
+
+            int x = rect_list.Count;
+
+            List<Point3d> footprint_corners = new List<Point3d>();
+            foreach (Curve seg in footprint.DuplicateSegments())
+            {
+                footprint_corners.Add(seg.PointAtStart);
+            }
+
+            List<Point3d> outside_corners = new List<Point3d>();
+
+            Vector3d vec = footprint_corners[0] - footprint_corners[1];
+
+
+
+            foreach (Rectangle3d room in rect_list)
+            {
+                List<Point3d> corner_list = new List<Point3d>();
+                for (int i = 0; i < 4; ++i)
+                {
+                    corner_list.Add(room.Corner(i));
+                }
+
+
+
+            }
+
+            DA.SetDataList(1, footprint_corners);
+
+        }
+
+
+        private Curve CreateSpiral(Plane plane, double r0, double r1, Int32 turns)
+        {
+            Line l0 = new Line(plane.Origin + r0 * plane.XAxis, plane.Origin + r1 * plane.XAxis);
+            Line l1 = new Line(plane.Origin - r0 * plane.XAxis, plane.Origin - r1 * plane.XAxis);
+
+            Point3d[] p0;
+            Point3d[] p1;
+
+            l0.ToNurbsCurve().DivideByCount(turns, true, out p0);
+            l1.ToNurbsCurve().DivideByCount(turns, true, out p1);
+
+            PolyCurve spiral = new PolyCurve();
+
+            for (int i = 0; i < p0.Length - 1; i++)
+            {
+                Arc arc0 = new Arc(p0[i], plane.YAxis, p1[i + 1]);
+                Arc arc1 = new Arc(p1[i + 1], -plane.YAxis, p0[i + 1]);
+
+                spiral.Append(arc0);
+                spiral.Append(arc1);
+            }
+
+            return spiral;
+        }
+
+        /// <summary>
+        /// The Exposure property controls where in the panel a component icon 
+        /// will appear. There are seven possible locations (primary to septenary), 
+        /// each of which can be combined with the GH_Exposure.obscure flag, which 
+        /// ensures the component will only be visible on panel dropdowns.
+        /// </summary>
+        public override GH_Exposure Exposure
+        {
+            get { return GH_Exposure.primary; }
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("bc8ae352-3d91-4a82-927e-c9396854e8aa"); }
+        }
+    }
+}
